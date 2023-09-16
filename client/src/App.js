d1257711246574650143ea9f79567f6bddb380ff
Zino Ebiala <eebia002@fiu.edu>
2023-09-15 23:58:17 -0400

    Added home component to App.js, and added starter code to Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from '../src/components/pages/Home'
+
 //Creating code
 function App() {
   return (
     <Router>
       <Routes>
-        <Route />
+        <Route  exact Component path={Home}/>
       </Routes>
     </Router>
   );
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from '../../App.css'
+import Navbar from '../Navbar.js'
+
+function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
