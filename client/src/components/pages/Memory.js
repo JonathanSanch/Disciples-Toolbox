48f43a15463f3d863f306c1996948c0250d0569d
Lucas <48874219+LucasTree2@users.noreply.github.com>
2023-09-17 10:07:57 -0400

    Finished Memory

--- client/src/components/pages/Memory.js
+++ client/src/components/pages/Memory.js
@@ -49,11 +49,29 @@ function Memory() {
     });
   };
 
-  const createMemoryScripture = () => {
+  
+  const apiKey = "c5d7d0f8da90d9a53978171558396b7400b9cba6";
+  const getScripture = async (book, ch, ver) => {
+    const query = book + "+" + ch + ":" + ver;
+    const apiUrl = `https://api.esv.org/v3/passage/text/?q=${query}&include-footnotes=false&wrapping-div=true`;
+
+    const response = await fetch(apiUrl, {
+        headers: {
+            Authorization: `Token ${apiKey}`
+        }
+    });
+    const data = await response.json();
+    return data.passages[0];
+  }
+
+  const createMemoryScripture = async () => {
+    let bibleText = await getScripture(inputValues.book, inputValues.chapter, inputValues.verses);
+
     const data = {
       book: inputValues.book,
       chapter: inputValues.chapter,
       verses: inputValues.verses,
+      text: bibleText
       // Add other properties as needed
     };
 
@@ -99,6 +117,14 @@ function Memory() {
     getMemoryScriptures();
   }, []);
 
+  const clearList = () => {
+    console.log("I totally cleared it")
+  }
+
+  const getRandomScripture = () => {
+    console.log("yuh");
+  }
+
   return (
     <Grid container spacing={2}>
       {/* Left Column */}
@@ -118,22 +144,130 @@ function Memory() {
             Memory Scripture:
           </Typography>
           <Typography variant="h6" marginTop="15px">
-            <div>
-              {/* Display the user-entered Memory Scripture */}
-              <p style={verseStyle}>{inputValues.book} {inputValues.chapter} {inputValues.startVerse} - {inputValues.endVerse}</p>
-            </div>
+            {(typeof memoryScriptures === 'undefined') ? (
+                <p></p>
+            ): (
+                <div>
+                    <p style={verseStyle}>{memoryScriptures[(Math.floor(Math.random() * (memoryScriptures.length - 1 + 1)) + 1) - 1].text}</p>
+                </div>
+            )}
           </Typography>
           <Typography style={{ textAlign: 'center' }}>
-            <Button className="cool-button" onClick={openModal}>
-              New Scripture
-            </Button>
-            <button className="cool-button">Add to Saved List</button>
+            <button className="cool-button" onClick={openModal}>New Scripture</button>
           </Typography>
         </Paper>
       </Grid>
       {/* Right Column */}
       <Grid item xs={12} md={6}>
       <Paper elevation={3} style={{ minHeight:'200px', borderRadius:'10px', backgroundColor:'#d0a4eb', color:'#594157', padding: '40px', margin: '45px' }}>
+          <Typography variant='h4' textAlign="center" style={typographyStyle}>
+              Saved List:
+              <button onClick={clearList} class="cool-button-2">Clear list</button>
+          </Typography> 
+          <Grid container spacing={2}>
+          {(typeof memoryScriptures === 'undefined') ? (
+                <p></p>
+            ): (
+                <>
+                    {memoryScriptures.map((item) => (
+                        <Listbox verseMessage={item.text} verseIndex={``}/>
+                    ))}
+                </>
+            )}
+          </Grid>
+        </Paper>
+      </Grid>
+      <Dialog open={isModalOpen} onClose={closeModal}>
+        <DialogTitle>Enter Memory Scripture Information</DialogTitle>
+        <DialogContent>
+          <TextField
+            name="book"
+            label="Book"
+            fullWidth
+            required
+            value={inputValues.book}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            name="chapter"
+            label="Chapter"
+            fullWidth
+            required
+            value={inputValues.chapter}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            name="verses"
+            label="Verse(s)"
+            fullWidth
+            required
+            value={inputValues.verses}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeModal} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => {
+                getScripture(inputValues.book, inputValues.chapter, inputValues.verses);
+                createMemoryScripture()
+            }} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
+}
+
+export default Memory;
+
+
+/*
+function Memory() {
+
+  const typographyStyle = {
+    fontFamily: 'Fugaz One', // Replace with your selected font family
+    marginBottom: '10px',
+  };
+
+  const verseStyle = {
+    fontStyle: 'italic',
+    marginBottom: '15px',
+  }
+
+  return (
+
+    <Grid container spacing={2}>
+      {/* Left Column *//*}
+      <Grid item xs={12} md={6}>
+        <Paper style={{ minHeight:'200px', borderRadius:'10px', padding: "40px", backgroundColor: "#594157", color:'#d0a4eb', margin:'45px'}} elevation={6}>
+          <Typography variant='h4' textAlign="center" style={typographyStyle}>
+              Memory Scripture:
+          </Typography>
+          <Typography variant='h6' marginTop='15px'>
+            <div>
+              {/* Hard coding current Memory Scripture. In the end, we want to get this from user. *//*}
+              <p style={verseStyle}>All glory to God, who is able, through His mighty power at work within us, to accomplish
+              infintely more than we might ask or think.</p>
+            </div>
+            <div style={{marginBottom:'15px'}}>
+              <p>Ephesians 3:20 NLT</p>
+            </div>
+          </Typography>
+          <Typography style={{textAlign: 'center'}}>
+            <button class="cool-button">New Scripture</button> 
+            <button class="cool-button">Add to Saved List</button>
+          </Typography>
+        </Paper>
+      </Grid>
+    {/* Right Column *//*}
+      <Grid item xs={12} md={6}>
+        <Paper elevation={3} style={{ minHeight:'200px', borderRadius:'10px', backgroundColor:'#d0a4eb', color:'#594157', padding: '40px', margin: '45px' }}>
           <Typography variant='h4' textAlign="center" style={typographyStyle}>
               Saved List:
               <button class="cool-button-2">Clear list</button>
@@ -145,45 +279,11 @@ function Memory() {
           </Grid>
         </Paper>
       </Grid>
-      <Dialog open={isModalOpen} onClose={closeModal}>
-        <DialogTitle>Enter Memory Scripture Information</DialogTitle>
-        <DialogContent>
-          <TextField
-            name="book"
-            label="Book"
-            fullWidth
-            value={inputValues.book}
-            onChange={handleInputChange}
-            margin="normal"
-          />
-          <TextField
-            name="chapter"
-            label="Chapter"
-            fullWidth
-            value={inputValues.chapter}
-            onChange={handleInputChange}
-            margin="normal"
-          />
-          <TextField
-            name="verses"
-            label="Verse(s)"
-            fullWidth
-            value={inputValues.verses}
-            onChange={handleInputChange}
-            margin="normal"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeModal} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={createMemoryScripture} color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Grid>
-  );
+
+  )
 }
 
-export default Memory;
+export default Memory
+
+*/
