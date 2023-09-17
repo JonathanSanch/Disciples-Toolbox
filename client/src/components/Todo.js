8317004420ab36ff27a9b62abc234405b3c071e2
Zino Ebiala <eebia002@fiu.edu>
2023-09-17 10:29:22 -0400

    done prayer

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -7,8 +7,24 @@ function Todo() {
   const [tasks, setTasks] = useState([]);
 
   const boxStyle = {
-    backgroundColor: '#354F52', 
-    borderRadius:'10px'
+    backgroundColor: '#499c91', 
+    marginTop: '30px',
+    marginBottom: '30px',
+    padding: '40px',
+    borderRadius:'10px',
+    minHeight: '300px',
+  }
+
+  const textFieldStyle = {
+    color: "white",
+    marginBottom: '20px',
+  }
+
+  const titleStyle = {
+    textAlign: "center",
+    color: "#CAD2C5",
+    fontFamily: "Fugaz One",
+    marginBottom: '30px',
   }
 
   const addTask = () => {
@@ -26,35 +42,51 @@ function Todo() {
   return (
     <Container maxWidth="md">
         <Paper style={boxStyle}>
-        <Typography variant='h3' textAlign="center" color="#CAD2C5"> Prrayer List</Typography>
-      <TextField
-        fullWidth
-        label="Add a Prayer Request"
-        variant="filled"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={addTask}
-      >
-        Add Task
-      </Button>
-      <List>
-        {tasks.map((t, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={t} />
+          <Typography variant='h3' style={titleStyle}> Prayer List</Typography>
+          <TextField
+            fullWidth
+            label="Add a Prayer Request"
+            variant="filled"
+            value={task}
+            style={textFieldStyle}
+            sx={{  color: '#63baa9' , borderRadius: '10px', backgroundColor: '#63baa9'}} 
+            onChange={(e) => setTask(e.target.value)}
+            InputProps={{
+                sx: {
+                    '&:hover fieldset': {
+                        border: '2px solid blue!important',
+                        borderRadius: 0,
+                    },
+                    '&:focus-within fieldset, &:focus-visible fieldset': {
+                        border: '4px solid red!important',
+                    }
+                }
+            }}
+          />
+          <Typography style={{textAlign: 'center'}}>
             <Button
-              variant="outlined"
-              style={{color: "#CAD2C5"}}
-              onClick={() => deleteTask(index)}
+                variant="contained"
+                onClick={addTask}
+                style={{backgroundColor: '#49c4ad', marginBottom: '5px'}}
+
             >
-              Delete
-            </Button>
-          </ListItem>
-        ))}
-      </List>
+                Add Prayer
+                </Button>
+            </Typography>
+          <List>
+            {tasks.map((t, index) => (
+              <ListItem key={index} style= {{backgroundColor: '#2e7067', color: "#CAD2C5", borderRadius: '10px', marginTop: '10px'}}>
+                <ListItemText primary={t} />
+                <Button
+                  variant="outlined"
+                  style={{color: '#2e7067', borderColor: 'black', border: '10px'}}
+                  onClick={() => deleteTask(index)}
+                >
+                  Delete
+                </Button>
+              </ListItem>
+            ))}
+          </List>
         </Paper>
     </Container>
   );
--- client/src/components/pages/Memory.js
+++ client/src/components/pages/Memory.js
@@ -1,20 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Grid,
-  Paper,
-  Typography,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
-} from '@mui/material';
-import '../../App.css';
+import React from 'react'
+import '../../App.css'
+import Grid from "@mui/material/Grid"
+import { Paper, Typography } from '@mui/material'
 import './Memory.css';
 import Listbox from '../Listbox.js';
 
 function Memory() {
+
   const typographyStyle = {
     fontFamily: 'Fugaz One', // Replace with your selected font family
     marginBottom: '10px',
@@ -23,117 +15,35 @@ function Memory() {
   const verseStyle = {
     fontStyle: 'italic',
     marginBottom: '15px',
-  };
-
-  const [memoryScriptures, setMemoryScriptures] = useState([]);
-  const [inputValues, setInputValues] = useState({
-    book: '',
-    chapter: '',
-    verses: '',
-  });
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setInputValues({
-      ...inputValues,
-      [name]: value,
-    });
-  };
-
-  const createMemoryScripture = () => {
-    const data = {
-      book: inputValues.book,
-      chapter: inputValues.chapter,
-      verses: inputValues.verses,
-      // Add other properties as needed
-    };
-
-    fetch("/memoryScripture", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to add Memory scripture");
-        }
-        console.log("Memory scripture added successfully");
-        setInputValues({
-          book: '',
-          chapter: '',
-          verses: '',
-          // Clear other fields if necessary
-        });
-        closeModal();
-        getMemoryScriptures();
-      })
-      .catch((error) => {
-        console.error("Error adding Memory scripture:", error);
-      });
-  };
-
-  const getMemoryScriptures = () => {
-    fetch("/memoryScripture")
-      .then((response) => response.json())
-      .then((data) => {
-        setMemoryScriptures(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching Memory scriptures:", error);
-      });
-  };
-
-  useEffect(() => {
-    getMemoryScriptures();
-  }, []);
+  }
 
   return (
+
     <Grid container spacing={2}>
       {/* Left Column */}
       <Grid item xs={12} md={6}>
-        <Paper
-          style={{
-            minHeight: '200px',
-            borderRadius: '10px',
-            padding: '40px',
-            backgroundColor: '#594157',
-            color: '#d0a4eb',
-            margin: '45px',
-          }}
-          elevation={6}
-        >
-          <Typography variant="h4" textAlign="center" style={typographyStyle}>
-            Memory Scripture:
+        <Paper style={{ minHeight:'200px', borderRadius:'10px', padding: "40px", backgroundColor: "#594157", color:'#d0a4eb', margin:'45px'}} elevation={6}>
+          <Typography variant='h4' textAlign="center" style={typographyStyle}>
+              Memory Scripture:
           </Typography>
-          <Typography variant="h6" marginTop="15px">
+          <Typography variant='h6' marginTop='15px'>
             <div>
-              {/* Display the user-entered Memory Scripture */}
-              <p style={verseStyle}>{inputValues.book} {inputValues.chapter} {inputValues.startVerse} - {inputValues.endVerse}</p>
+              <p style={verseStyle}>All glory to God, who is able, through His mighty power at work within us, to accomplish
+              infintely more than we might ask or think.</p>
+            </div>
+            <div style={{marginBottom:'15px'}}>
+              <p>Ephesians 3:20 NLT</p>
             </div>
           </Typography>
-          <Typography style={{ textAlign: 'center' }}>
-            <Button className="cool-button" onClick={openModal}>
-              New Scripture
-            </Button>
-            <button className="cool-button">Add to Saved List</button>
+          <Typography style={{textAlign: 'center'}}>
+            <button class="cool-button">New Scripture</button>
+            <button class="cool-button">Add to Saved List</button>
           </Typography>
         </Paper>
       </Grid>
-      {/* Right Column */}
+    {/* Right Column */}
       <Grid item xs={12} md={6}>
-      <Paper elevation={3} style={{ minHeight:'200px', borderRadius:'10px', backgroundColor:'#d0a4eb', color:'#594157', padding: '40px', margin: '45px' }}>
+        <Paper elevation={3} style={{ minHeight:'200px', borderRadius:'10px', backgroundColor:'#d0a4eb', color:'#594157', padding: '40px', margin: '45px' }}>
           <Typography variant='h4' textAlign="center" style={typographyStyle}>
               Saved List:
               <button class="cool-button-2">Clear list</button>
@@ -145,45 +55,9 @@ function Memory() {
           </Grid>
         </Paper>
       </Grid>
-      <Dialog open={isModalOpen} onClose={closeModal}>
-        <DialogTitle>Enter Memory Scripture Information</DialogTitle>
-        <DialogContent>
-          <TextField
-            name="book"
-            label="Book"
-            fullWidth
-            value={inputValues.book}
-            onChange={handleInputChange}
-            margin="normal"
-          />
-          <TextField
-            name="chapter"
-            label="Chapter"
-            fullWidth
-            value={inputValues.chapter}
-            onChange={handleInputChange}
-            margin="normal"
-          />
-          <TextField
-            name="verses"
-            label="Verse(s)"
-            fullWidth
-            value={inputValues.verses}
-            onChange={handleInputChange}
-            margin="normal"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeModal} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={createMemoryScripture} color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Grid>
-  );
+
+  )
 }
 
-export default Memory;
+export default Memory
